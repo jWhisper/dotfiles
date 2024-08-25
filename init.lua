d4fca67e4d5c75c10ed10c69da8dbf265f71091d
xujinwen <xjweny@163.com>
2024-08-26 01:16:57 +0800

    add todo

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ require("options")
 require("keymaps")
 require("autocmd")
 
+--TODO:
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -9,7 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
+		-- error("Error cloning lazy.nvim:\n" .. out)
 	end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
@@ -27,6 +29,13 @@ require("lazy").setup({
 		config = function()
 			-- vim.cmd([[colorscheme tokyonight-night]])
 		end,
+	},
+
+	{
+		"folke/todo-comments.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
 	},
 
 	{ import = "plugins" },
