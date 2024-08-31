c6b5cf1f7876ab44b554a1436e7f13b4668ac13e
wyman <33257101+jWhisper@users.noreply.github.com>
2024-08-31 21:59:54 +0800

    Update custom.zsh

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -1,14 +1,17 @@
+# zsh
+source $HOME/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $HOME/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+source $HOME/.config/zsh/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+bindkey              '^I'         menu-complete
+bindkey "$terminfo[kcbt]" reverse-menu-complete
+
+source $HOME/.config/zsh/zsh-z/zsh-z.plugin.zsh
+
 # Starship
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 eval "$(starship init zsh)"
 starship config palette $STARSHIP_THEME
-
-
-# zsh
-source $HOME/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source $HOME/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $HOME/.config/zsh/zsh-autocomplete/zsh-autocomplete.plugin.zsh
-source $HOME/.config/zsh/zsh-z/zsh-z.plugin.zsh
 
 # fzf
 source <(fzf --zsh)
